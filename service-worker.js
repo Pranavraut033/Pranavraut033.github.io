594493032a17b0cb43be37111199b596dbbf0010
Pranav Raut <pranavraut033@gmail.com>
2019-07-02 11:18:49 +0530

    Created project section

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f40adfc65fb2baddca350bf105c9c2b2.js"
+  "/precache-manifest.c33d1ea66bc5ee84f9a5302b6a9c7fc6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "portfolio"});
